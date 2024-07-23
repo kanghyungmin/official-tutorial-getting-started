a1fcd4d5600950d1aea14adf21a71e6ca2a88a73
hyungmin <hmkkang0922@daum.net>
2024-07-23 13:07:17 +0900

    modify readme file

--- test.ts
+++ test.ts
@@ -1,35 +1,33 @@
 
-// 1. leetcode 3개 풀기
-// 2. nestjs DDD best praticess
+// 1. leetcode 1개 풀기
+// 2. go 2개 투토리얼 확인
+// 3. nestjs DDD best praticess
 // - https://github.com/kanghyungmin/domain-driven-hexagon?tab=readme-ov-file
-// 3. go 2개 투토리얼 확인
+//
 
 
-
-//   Definition for a binary tree node.
-function luckyNumbers (matrix: number[][]): number[] {
-    let minValues : number[] =[], maxValues : number[] = []
-    let [row, col] = [matrix.length , matrix[0].length ]
-    for(let i=0; i<row;i++) {
-        minValues.push(Math.min(...matrix[i]))
-    }
-
-    for(let i=0; i<col;i++) {
-        let temp = 0;
-        for(let j=0; j<row;j++) {
-            temp = Math.max(matrix[j][i], temp)
-        }
-        maxValues.push(temp)
-    }
-    let retVal : number[] =[]
-
-    minValues.map( v => {
-        if(maxValues.includes(v))
-            retVal.push(v);
+function sortPeople(names: string[], heights: number[]): string[] {
+    let mappedArr : Array<{height: number; name: string}> = names.map((name, index) => ({
+        height : heights[index],
+        name : name,
+    }))
+    mappedArr.sort( (a,b,) => {
+        if(a.height < b.height)
+            return 1;
+        return -1
     })
 
-    return retVal as number[]
+    let retVal : string[] = []
+    for(const i of mappedArr.values()) {
+        retVal.push(i.name)
+    }
+
+    return retVal
 };
-  
-let matrix = [[1,10,4,2],[9,3,8,7],[15,16,17,12]]
-console.log(luckyNumbers(matrix))
+
+
+
+let names = ["Mary","John","Emma"], heights = [180,165,170]
+console.log(sortPeople(names,heights))
+names = ["Alice","Bob","Bob"], heights = [155,185,150]
+console.log(sortPeople(names,heights))
