c225a07f9b06444258050cc3c47ded823d00b2ab
hyungmin <hmkkang0922@daum.net>
2024-07-25 10:09:48 +0900

    add git ignore file

--- test.ts
+++ test.ts
@@ -2,32 +2,42 @@
 // 1. leetcode 1개 풀기
 // 2. go 2개 투토리얼 확인
 // 3. nestjs DDD best praticess
-// - https://github.com/kanghyungmin/domain-driven-hexagon?tab=readme-ov-file
-//
+// - https://khalilstemmler.com/articles/categories/domain-driven-design/ (6/17)
+// - https://junho2343.github.io/posts/clean-architecture-hexagonal-architecture-with-nestjs
+// - https://eocoding.tistory.com/36
 
 
-function sortPeople(names: string[], heights: number[]): string[] {
-    let mappedArr : Array<{height: number; name: string}> = names.map((name, index) => ({
-        height : heights[index],
-        name : name,
-    }))
-    mappedArr.sort( (a,b,) => {
-        if(a.height < b.height)
-            return 1;
-        return -1
-    })
+function frequencySort(nums: number[]): number[] {
+    const freqMap = new Map<number, number>();
 
-    let retVal : string[] = []
-    for(const i of mappedArr.values()) {
-        retVal.push(i.name)
-    }
+    // Count the frequency of each number in the array
+    for (const num of nums) {
+        freqMap.set(num, (freqMap.get(num) || 0) + 1)
+        
+        }
 
-    return retVal
-};
+    // Sort the array based on frequency (increasing) and then by value (decreasing)
+    nums.sort((a, b) => {
+        const freqA = freqMap.get(a)!;
+        const freqB = freqMap.get(b)!;
+        
+        // If frequencies are different, sort by frequency
+        if (freqA !== freqB) {
+            return freqA - freqB;
+        }
+        
+        // If frequencies are the same, sort by value in decreasing order
+        return b - a;
+    });
+
+    return nums;
+}
+
+let nums = [-1,1,-6,4,5,-6,1,4,1]
+// nums=[1,1,2,2,2,3]
+
+console.log(frequencySort(nums)); // [1,3,3,2,2]
 
 
 
-let names = ["Mary","John","Emma"], heights = [180,165,170]
-console.log(sortPeople(names,heights))
-names = ["Alice","Bob","Bob"], heights = [155,185,150]
-console.log(sortPeople(names,heights))
+
